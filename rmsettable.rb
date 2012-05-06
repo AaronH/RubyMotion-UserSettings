df3be0622ad613d31b5a5e75572fb0720a7710f0
Aaron Hurley <aaron@aaronhurley.com>
2012-05-06 16:45:07 -0700

    Initial commit. Fully functional and well documented. Needs tests and a real README

---
+++ rmsettable.rb
@@ -0,0 +1,94 @@
+module RMSettable
+
+  def rm_settable(*args)
+    # Call this in your app_delegate.rb as part of the didFinishLaunchingWithOptions setup.
+    #
+    # The arguments can consist of symbols for various settings you wish to store.
+    #
+    #    rm_settable :name, :background_color
+    #
+    #
+    # By default, the keys expect the stored settings to be passed and stored as strings.
+    # If you wish to use another type, you may declare that as a hash on the key instead.
+    #
+    #    rm_settable :name, age: {type: :integer}, superstar: {type: :boolean}
+    #
+    # AVAILABLE TYPES :array, :boolean, :data, :dictionary, :double, :float,
+    #                 :integer, :object, :string_array, :string, :url
+    #
+    #
+    # You can also include a hash of options.
+    #
+    #    rm_settable name: {type: :string}, age: options: {autosave: false, default_type: :integer}
+    #
+    #    rm_settable options: {:lenient_keys}
+    #
+    # AVAILBLE OPTIONS
+    #        :autosave  true (default)
+    #                   By default, settings are synchronized to the device as soon as
+    #                   it is updated. However, if you are updating a lot of items at once
+    #                   or would prefer to manually call save on the settings object, set
+    #                   this to false. Be aware that iOS will still save the settings
+    #                   periodically without being explicitly called.
+    #
+    #    :default_type  :string (default)
+    #                   Items being saved expect to be strings. You many specify the type
+    #                   when you declare the setting names or override the default
+    #                   expected for all objects.
+    #
+    #    :lenient_keys  false (default)
+    #                   If you don't want to declare any or all of the settings you will
+    #                   be storing, set this to true. Any setting not explicitly declared
+    #                   will be stored and retrieved as the default setting type.
+    #
+    @rm_settings = RMSettings.new args
+  end
+
+
+  def applicationDidEnterBackground(application)
+    # make sure we save any unsaved settings when going to the background
+    # if you override this method in the app_delegate
+    # be sure to call super to save any unsaved settings
+    rm_settings.save
+  end
+
+  def applicationWillTerminate(application)
+    # make sure we save any unsaved settings when being terminated
+    # if you override this method in the app_delegate
+    # be sure to call super to save any unsaved settings
+    rm_settings.save
+  end
+
+  def settings
+    # provide access to the RMSettings object to the app_delegate
+    rm_settings
+  end
+
+  def self.included(base)
+    base.class_eval do
+      attr_accessor :rm_settings
+    end
+  end
+end
+
+# These subclasses provide easy access to the app_delegate and settings object.
+# If you wish to call settings directly from your controller or view, be sure
+# to inherit from these classes
+class RMView < UIView
+  def settings
+    application.settings
+  end
+
+  def application
+    UIApplication.sharedApplication.delegate
+  end
+end
+
+class RMViewController < UIViewController
+  def settings
+    application.settings
+  end
+  def application
+    UIApplication.sharedApplication.delegate
+  end
+end
---
+++ rmsettings.rb
@@ -0,0 +1,178 @@
+class RMSettings
+  attr_accessor :settings, :available, :keys, :options
+
+  def initialize(*args)
+    # Initializes the available settings and options
+    # See rmsettable.rb for details on the various options
+    args = args.flatten.compact.uniq
+    @available  = {}
+    @settings ||= NSUserDefaults.standardUserDefaults
+
+    user_options = (args.select{|a| a.is_a? Hash}.first || {}).delete(:options) || {}
+    @options  = { autosave:     true,
+                  lenient_keys: false,
+                  default_type: :string}.merge(user_options)
+
+    args.flatten.each do |item|
+      if item.is_a?(Symbol)
+        @available[item] = {type: @options[:default_type]}
+      elsif item.is_a?(Hash)
+        item.each do |key, value|
+          @available[key] = value
+        end
+      end
+    end
+    @keys = @available.keys
+  end
+
+  def save
+    # Save all current settings to the device.
+    # Can be called manually but is generally called automatically
+    # after updating a key unless the :autosave option is false.
+    puts "Saving"
+    settings.synchronize
+  end
+
+  def autosave
+    # Automatically saves the data unless autosave is false.
+    save if autosave?
+  end
+
+  def autosave?
+    # Check to see if the autosave option is selected
+    options[:autosave]
+  end
+
+  def default_type_for(key)
+    # Determine the appropriate type for reading and writing item
+    ((@available[key] || {})[:type] || @options[:default_type])
+  end
+
+  def setting_for?(key)
+    # Call to check boolean status of a particular key.
+    # Generally called dynamically from the method_missing function.
+    #
+    #    settings.setting_for? :tutorial_completed
+    #
+    #    settings.tutorial_completed?
+    #
+    value = setting_for(key)
+    case default_type_for(key)
+    when :string, :array
+      value.empty?
+    else
+      !!value
+    end
+  end
+
+  def save_setting(key, value)
+    # Save the contents of value to the key.
+    # Returns the value of the key after setting.
+    #
+    # Generally called dynamically from the method_missing function.
+    #
+    #    settings.save_setting :name, 'Karl Pilkington'
+    #
+    #    settings.head_shape = 'Orange'
+    #
+    case default_type_for(key)
+    when :boolean
+      settings.setBool    value, forKey: key
+    when :double
+      settings.setDouble  value, forKey: key
+    when :float
+      settings.setFloat   value, forKey: key
+    when :integer
+      settings.setInteger value, forKey: key
+    when :object
+      settings.setObject  value, forKey: key
+    when :url
+      settings.setURL     value, forKey: key
+    else
+      settings.setObject  value, forKey: key
+    end
+
+    autosave
+
+    # return the current value
+    setting_for key
+  end
+
+  def setting_for(key)
+    # Read the contents of a given key.
+    #
+    # Generally called dynamically from the method_missing function.
+    #
+    #    settings.setting_for :nickname
+    #
+    #    settings.nickname
+    #
+    case default_type_for(key)
+    when :array
+      settings.arrayForKey      key
+    when :boolean
+      settings.boolForKey       key
+    when :data
+      settings.dataForKey       key
+    when :dictionary
+      settings.dictionaryForKey key
+    when :double
+      settings.doubleForKey     key
+    when :float
+      settings.floatForKey      key
+    when :integer
+      settings.integerForKey    key
+    when :object
+      settings.objectForKey     key
+    when :string_array
+      settings.stringArrayForKey key
+    when :string
+      settings.stringForKey     key
+    when :url
+      settings.URLForKey        key
+    end
+  end
+
+  def remove(key)
+    # Resets the key to it's default value and returns that value
+    # Aliased as delete and reset
+    settings.removeObjectForKey key
+    autosave
+    setting_for key
+  end
+  alias :delete :remove
+  alias :reset  :remove
+
+  def valid_key?(key)
+    # Checks to see if the method_missing key has been declared
+    # unless that functionality has been turned off in the settings
+    @options[:lenient_keys] or keys.include?(key)
+  end
+
+  def method_missing(method, *args)
+    # Split the method to componenet parts to do our magic.
+    if m = method.to_s.match(/^([^\?\=]+)([\?\=])?$/)
+      base_key        = m[1].to_sym
+      method_modifier = m[2]
+      # Check if we want to do something here.
+      if valid_key?(base_key)
+        return  case method_modifier
+                when nil
+                  # Call the reader
+                  setting_for(base_key)
+                when '='
+                  # Call the writer
+                  save_setting(base_key, [*args].flatten.first)
+                when '?'
+                  # Call the boolean check
+                  setting_for?(base_key)
+                else
+                  # Nothing to do, so let's fail
+                  super
+                end
+      end
+    end
+    super
+  end
+
+end
